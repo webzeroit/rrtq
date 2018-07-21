fd88b0f35f34d6fae4877e9ff864ebb806ebf897
rlanzetta <info@webzero.it>
2018-07-21 18:26:57 +0200

    Gestito profilo Supervisore con funzionalità Approva Revisione. Tolti target="_blank" per problema su FIREFOX

--- application/helpers/my_datatable_helper.php
+++ application/helpers/my_datatable_helper.php
@@ -26,36 +26,45 @@ if (!function_exists('dt_profilo_action'))
         }
         //SE PUBBLICATO 
         /* GESTIONE RUOLI */
-        $resp_usr = $ci->config->item('role_responsabile');
-        if ($ci->ion_auth->is_admin() || $ci->ion_auth->in_group($resp_usr))
+        if ($ci->ion_auth->is_admin() || 
+            $ci->ion_auth->in_group($ci->config->item('role_responsabile')) 
+        )
         {
+            if ($stato == 0)
+            {
+                $action_link .= '<a href="javascript:sospendi_pubblicazione(' . $id . ');" data-toggle="tooltip" data-original-title="Sospendi Pubblicazione"><i class="fa fa-chain-broken text-inverse m-r-5"></i></a>';
+            }
+            // SE REVISIONE APPROVATA
             if ($stato == 1)
             {
-                $action_link .= '<a href="javascript:sospendi_pubblicazione(' . $id . ');" target="_blank" data-toggle="tooltip" data-original-title="Sospendi Pubblicazione"><i class="fa fa-chain-broken text-inverse m-r-5"></i></a>';
-            }
-            //SE REVISIONE
-            if ($stato == 2)
-            {
-                $action_link .= '<a href="javascript:sospendi_pubblicazione(' . $id . ');" target="_blank" data-toggle="tooltip" data-original-title="Sospendi Pubblicazione"><i class="fa fa-chain-broken text-inverse m-r-5"></i></a>';
-                $action_link .= '<a href="javascript:avvia_pubblicazione(' . $id . ');" target="_blank" data-toggle="tooltip" data-original-title="Pubblica"><i class="fa fa-globe text-inverse m-r-5"></i></a>';
-            }
+                $action_link .= '<a href="javascript:avvia_pubblicazione(' . $id . ');" data-toggle="tooltip" data-original-title="Pubblica"><i class="fa fa-globe text-inverse m-r-5"></i></a>';
+            }           
             //NON PUBBLICATO
             if ($stato == 3)
             {
-                $action_link .= '<a href="javascript:avvia_pubblicazione(' . $id . ');" target="_blank" data-toggle="tooltip" data-original-title="Pubblica"><i class="fa fa-globe text-inverse m-r-5"></i></a>';
+                $action_link .= '<a href="javascript:avvia_pubblicazione(' . $id . ');" data-toggle="tooltip" data-original-title="Pubblica"><i class="fa fa-globe text-inverse m-r-5"></i></a>';
             }
             //SE NON CANCELLATO
             if ($stato != 4)
             {
-                $action_link .= '<a href="javascript:elimina_pubblicazione(' . $id . ');" target="_blank" data-toggle="tooltip" data-original-title="Elimina"><i class="fa fa-close text-danger m-r-5"></i></a>';
+                $action_link .= '<a href="javascript:elimina_pubblicazione(' . $id . ');" data-toggle="tooltip" data-original-title="Elimina"><i class="fa fa-close text-danger m-r-5"></i></a>';
             }
         }
+        if ($ci->ion_auth->is_admin() || 
+            $ci->ion_auth->in_group($ci->config->item('role_supervisore')))
+        {
+            //SE REVISIONE LA PUO' APPROVARE
+            if ($stato == 2)
+            {                
+                $action_link .= '<a href="javascript:approva_revisione(' . $id . ');" data-toggle="tooltip" data-original-title="Approva Revisione"><i class="fa fa-check-square-o text-inverse m-r-5"></i></a>';
+            }  
+         }
         if ($ci->ion_auth->is_admin())
         {
             if ($stato == 4)
             {
-                $action_link .= '<a href="javascript:elimina_qualificazione(' . $id . ');" target="_blank" data-toggle="tooltip" data-original-title="Elimina Definitivamente"><i class="fa fa-trash-o text-danger m-r-5"></i></a>';
-                $action_link .= '<a href="javascript:ripristina_qualificazione(' . $id . ');" target="_blank" data-toggle="tooltip" data-original-title="Ripristina"><i class="fa fa-undo text-inverse m-r-5"></i></a>';
+                $action_link .= '<a href="javascript:elimina_qualificazione(' . $id . ');" data-toggle="tooltip" data-original-title="Elimina Definitivamente"><i class="fa fa-trash-o text-danger m-r-5"></i></a>';
+                $action_link .= '<a href="javascript:ripristina_qualificazione(' . $id . ');" data-toggle="tooltip" data-original-title="Ripristina"><i class="fa fa-undo text-inverse m-r-5"></i></a>';
             }
         }
         return $action_link;
@@ -71,12 +80,14 @@ if (!function_exists('dt_uc_action'))
         $ci = & get_instance();
         $action_link = '<a href="unitacompetenza/gestione/' . $id . '" data-toggle="tooltip" data-original-title="Gestione"> <i class="fa fa-edit text-inverse m-r-5"></i> </a>';
         
-        $resp_usr = $ci->config->item('role_responsabile');
-        if ($ci->ion_auth->is_admin() || $ci->ion_auth->in_group($resp_usr))
+        if ($ci->ion_auth->is_admin() || 
+            $ci->ion_auth->in_group($ci->config->item('role_responsabile')) || 
+            $ci->ion_auth->in_group($ci->config->item('role_supervisore'))                    
+        )
         {
             if ($profili_associati == 0)
             {
-                $action_link .= '<a href="javascript:elimina_competenza(' . $id . ');" target="_blank" data-toggle="tooltip" data-original-title="Elimina"><i class="fa fa-close text-danger m-r-5"></i></a>';
+                $action_link .= '<a href="javascript:elimina_competenza(' . $id . ');"  data-toggle="tooltip" data-original-title="Elimina"><i class="fa fa-close text-danger m-r-5"></i></a>';
             }
         }
         return $action_link;
@@ -93,8 +104,10 @@ if (!function_exists('dt_abilita_action'))
         $ci = & get_instance();
         $action_link = '<a href="abilita/gestione/' . $id . '" data-toggle="tooltip" data-original-title="Gestione"> <i class="fa fa-edit text-inverse m-r-5"></i> </a>';
         
-        $resp_usr = $ci->config->item('role_responsabile');
-        if ($ci->ion_auth->is_admin() || $ci->ion_auth->in_group($resp_usr))
+        if ($ci->ion_auth->is_admin() || 
+            $ci->ion_auth->in_group($ci->config->item('role_responsabile')) || 
+            $ci->ion_auth->in_group($ci->config->item('role_supervisore'))                    
+        )
         {
             if ($competenze_associate == 0)
             {
@@ -114,8 +127,10 @@ if (!function_exists('dt_conoscenza_action'))
         $ci = & get_instance();
         $action_link = '<a href="conoscenza/gestione/' . $id . '" data-toggle="tooltip" data-original-title="Gestione"> <i class="fa fa-edit text-inverse m-r-5"></i> </a>';
         
-        $resp_usr = $ci->config->item('role_responsabile');
-        if ($ci->ion_auth->is_admin() || $ci->ion_auth->in_group($resp_usr))
+        if ($ci->ion_auth->is_admin() || 
+            $ci->ion_auth->in_group($ci->config->item('role_responsabile')) || 
+            $ci->ion_auth->in_group($ci->config->item('role_supervisore'))                    
+        )
         {
             if ($competenze_associate == 0)
             {
