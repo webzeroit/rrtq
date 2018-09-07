a291b0c5da69cad6815f64ea80317b0298937d90
rlanzetta <info@webzero.it>
2018-09-07 18:11:38 +0200

    Release 1.3

--- application/hooks/hook.maintenance.php
+++ application/hooks/hook.maintenance.php
@@ -15,10 +15,34 @@ class MaintenanceEnabler
 
             include(APPPATH.'config/app_config.php');
             
-            if(!in_array($_SERVER['REMOTE_ADDR'], $config['maintenance_ips']) && $config['maintenance_mode'] === TRUE) {
+            $client_ip = $this->get_client_ip_env();
+            
+            if(!in_array($client_ip, $config['maintenance_ips']) && $config['maintenance_mode'] === TRUE) {
                 include(APPPATH.'views/manutenzione.php');
                 exit;
             }
         }
     }
+    
+    private function get_client_ip_env()
+    {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if (getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if (getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if (getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if (getenv('HTTP_FORWARDED'))
+            $ipaddress = getenv('HTTP_FORWARDED');
+        else if (getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+
+        return $ipaddress;
+    }
+    
 }
